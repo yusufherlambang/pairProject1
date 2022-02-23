aa51215fea36192ed8a6c558605fb815e7952a3b
Dimas Kurniawan <ichbindimas@gmail.com>
2022-02-24 01:09:37 +0800

    done migrate

--- migrations/20220223155109-create-profile.js
+++ migrations/20220223155109-create-profile.js
@@ -18,7 +18,13 @@ module.exports = {
       },
       UserId: {
         allowNull: false,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onUpdate: 'cascade',
+        onDelete: 'cascade'
       },
       createdAt: {
         allowNull: false,
--- migrations/20220223155157-add-UserId-to-ShoppingCart.js
+++ migrations/20220223155157-add-UserId-to-ShoppingCart.js
@@ -1,24 +1,23 @@
 'use strict';
 
 module.exports = {
-  
-  up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-    return queryInterface.createTable('ShoppingCart',)
+
+  up(queryInterface, Sequelize) {
+
+    return queryInterface.addColumn('ShoppingCarts', 'UserId', {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'Users',
+        key: 'id'
+      },
+      onUpdate: 'cascade',
+      onDelete: 'cascade'
+    })
   },
 
-  
-  down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+
+  down(queryInterface, Sequelize) {
+
+    return queryInterface.removeColumn('ShoppingCarts', 'UserId')
   }
 };
